05309c99eeb15be5e4f9dd38a2f064b4319ab8af
Parveengill02 <parveenkaurgill02@gmail.com>
2025-02-13 13:58:11 +0530

    My second commit

--- src/pages/Vendors/index.jsx
+++ src/pages/Vendors/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Accordion } from 'react-bootstrap';
 function Vendorpage() {
   const vendors = [
     {
@@ -127,7 +127,162 @@ function Vendorpage() {
   <div class="row">
     <div class="col-md-3">
       <div className='SideFilter'>
-      <h5>Find a venue</h5>
+      <div className='filterhead'>
+        <h5>Filters</h5>
+        <img src="/images/filterss.png"/>
+        <button type='clear'>Clear</button>
+       </div>
+       <Accordion>
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>Loaction</Accordion.Header>
+                    <Accordion.Body>
+                    <div className="scrollable-list">
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Mumbai" />
+                          <label className="form-check-label" htmlFor="Mumbai">Mumbai</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Delhi" />
+                          <label className="form-check-label" htmlFor="Delhi">Delhi</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Bengaluru" />
+                          <label className="form-check-label" htmlFor="Bengaluru">Bengaluru</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Hyderabad" />
+                          <label className="form-check-label" htmlFor="Hyderabad">Hyderabad</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Chennai" />
+                          <label className="form-check-label" htmlFor="Chennai">Chennai</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Kolkata" />
+                          <label className="form-check-label" htmlFor="Kolkata">Kolkata</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Jaipur" />
+                          <label className="form-check-label" htmlFor="Jaipur">Jaipur</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Pune" />
+                          <label className="form-check-label" htmlFor="Pune">Pune</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Goa" />
+                          <label className="form-check-label" htmlFor="Goa">Goa</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Kochi" />
+                          <label className="form-check-label" htmlFor="Kochi">Kochi</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Ahmedabad" />
+                          <label className="form-check-label" htmlFor="Ahmedabad">Ahmedabad</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Surat" />
+                          <label className="form-check-label" htmlFor="Surat">Surat</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Lucknow" />
+                          <label className="form-check-label" htmlFor="Lucknow">Lucknow</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Chandigarh" />
+                          <label className="form-check-label" htmlFor="Chandigarh">Chandigarh</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Indore" />
+                          <label className="form-check-label" htmlFor="Indore">Indore</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Ludhiana" />
+                          <label className="form-check-label" htmlFor="Ludhiana">Ludhiana</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Bhopal" />
+                          <label className="form-check-label" htmlFor="Bhopal">Bhopal</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Coimbatore" />
+                          <label className="form-check-label" htmlFor="Coimbatore">Coimbatore</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Visakhapatnam" />
+                          <label className="form-check-label" htmlFor="Visakhapatnam">Visakhapatnam</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Ranchi" />
+                          <label className="form-check-label" htmlFor="Ranchi">Ranchi</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Jodhpur" />
+                          <label className="form-check-label" htmlFor="Jodhpur">Jodhpur</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Agra" />
+                          <label className="form-check-label" htmlFor="Agra">Agra</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Varanasi" />
+                          <label className="form-check-label" htmlFor="Varanasi">Varanasi</label>
+                        </div>
+                        <div className="form-check">
+                          <input className="form-check-input" type="checkbox" id="Rishikesh" />
+                          <label className="form-check-label" htmlFor="Rishikesh">Rishikesh</label>
+                        </div>
+                      </div>
+                      <button class="btn btn-primary mt-3">Done</button>
+
+                    </Accordion.Body>
+                  </Accordion.Item>
+                  <Accordion.Item eventKey="1">
+                    <Accordion.Header>Posted by</Accordion.Header>
+                    <Accordion.Body>
+                      <ul className='buttongap'>
+                        <li> <button> + Owner</button></li>
+                        <li>  <button> + Builder</button></li>
+                        <li><button> + Dealer </button></li>
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                  <Accordion.Item eventKey="2">
+                    <Accordion.Header>Localities</Accordion.Header>
+                    <Accordion.Body>
+                      <ul className='buttongap'>
+                        <li> <button> + West Mumbai </button></li>
+                        <li>  <button> + East Mumbai</button></li>
+                        <li><button> +  Prabhadevi, Mumbai  </button></li>
+                        <li><button> + Chembur East, Mumbai </button></li>
+                        <li><button> + Pirojshanagar, Vikhroli East, Mumbai</button></li>
+                        <li><button> + Juhu, Mumbai </button></li>
+                        <li><button> + Titwala Thane, Mumbai  </button></li>
+                        <li><button> +  Goregaon West, Mumbai  </button></li>
+                        <li><button> +   Parel, Mumbai   </button></li>
+                        <li><button> +   Jogeshwari East, Mumbai   </button></li>
+                        <li><button> +  Byculla, Mumbai </button></li>
+                        <li><button> +  Mahalaxmi, Mumbai   </button></li>
+
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                  <Accordion.Item eventKey="3">
+                    <Accordion.Header>Amenities</Accordion.Header>
+                    <Accordion.Body>
+                      <ul className='buttongap'>
+                        <li> <button> + Parking</button></li>
+                        <li>  <button> + Park</button></li>
+                        <li><button> + Gas pipeline </button></li>
+                        <li><button> + Power Backup </button></li>
+                        <li><button> + Security Personnel </button></li>
+                      </ul>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+      
+
       </div>
       <div class='container'></div>
     </div>
